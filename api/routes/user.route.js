a24356d2046f9446dba9715516b9e1db614cdb7a
Sadok <kouassisadok3@gmail.com>
2024-10-16 16:32:03 +0000

    update user api routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,14 @@
-
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import {
+  test,
+  updateUser,
+} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-
-router.post('/signup', signup);
+router.get('/test', test);
+router.put('/update/:userId', verifyToken, updateUser);
 
 
 export default router;
